f8cc80729e7cb764112ede944b01efdd9749f71b
ericnost <ericnost@gmail.com>
2020-06-24 15:12:45 -0400

    report-ready version
    Fixes #28 (two months)

--- utilities.py
+++ utilities.py
@@ -32,16 +32,14 @@ def charter (full_data, date_column, date_format, mnth_name,
     # Organize the data
     this_data = full_data.groupby([date_column])[counting_column].agg(function) # For each day, count the number of inspections/enforcements/violations # Summarize inspections/enforcements/violations on a monthly basis  
     this_data = this_data.loc[((this_data.index.month == 3) | (this_data.index.month == 4))  & (this_data.index.year >= 2001)] # Filter to higher quality data timeframe (post-2001) and back to just the selected month 
-    this_data = this_data.resample('Y').sum()
+    this_data = this_data.resample('Y').sum() # Add together the two months (3 - 4) we're looking at
     this_data = pd.DataFrame(this_data) # Put our data into a dataframe
     this_data = this_data.rename(columns={counting_column: measure}) # Format the data columns
     this_data.index = this_data.index.strftime('%Y') # Make the x axis (date) prettier
 
     # Create the chart
-    ax = this_data.plot(kind='bar', title = title, figsize=(20, 10), fontsize=16, color=colour)
+    ax = this_data.plot(kind='bar', title = ""+title+" in %s of each year across ECHO history" %(mnth_name), figsize=(20, 10), fontsize=16, color=colour)
     ax
-
-    print(this_data)
 
     # Label trendline
     trend=this_data[measure].mean()
@@ -50,12 +48,12 @@ def charter (full_data, date_column, date_format, mnth_name,
     # Label the previous three years' trend (2017, 2018, 2019)
     trend_month=pd.concat([this_data.loc["2017"],this_data.loc["2018"],this_data.loc["2019"]])
     trend_month=trend_month[measure].mean()
-    ax.axhline(y=trend_month, xmin = .82, xmax=.93, color='#d43a69', linestyle='--', label = "%s 2017-2019 average" %(mnth_name))
+    ax.axhline(y=trend_month, xmin = .82, xmax=.93, color='#d43a69', linestyle='--', label = "Average for %s 2017-2019" %(mnth_name))
 
     # Label plot
     ax.legend()
     ax.set_xlabel("March and April of Each Year")
-    ax.set_ylabel( title)
+    ax.set_ylabel(title)
 
     # Export data 
     this_data.to_csv(filename) # This will make all of our charts for us
